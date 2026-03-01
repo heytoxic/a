85518a21b67891f8bd00d5d2b625dcefafbab9cb
heytoxic <whytoxicz@gmail.com>
2026-03-01 23:15:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def generate_report(query, query_id, user_id):
                 display_key = str(column_name).strip()
                 col_lower = display_key.lower()
                 
-                if col_lower == "name":
+                if col_lower == "father":
                     display_key = "Name"
                 elif col_lower == "fullname" in col_lower:
                     display_key = "Father Name"
